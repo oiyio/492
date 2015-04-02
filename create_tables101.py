dc1e080a9a52c81ba7acc428d87473a8c7d02f11
oiyio <omeriyioz@gmail.com>
2015-04-02 14:51:31 +0300

    database creation is done firstl

---
+++ create_tables101.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import sqlite3
+
+def create():
+	db = sqlite3.connect("mydb.db")
+
+	im = db.cursor()
+
+	im.execute("""CREATE TABLE users(
+					username VARCHAR(20) NOT NULL, 
+					password VARCHAR(20) NOT NULL)""")
+
+	veriler = [
+				("ahmet123", "12345678"),
+				("mehmet321", "87654321"),
+				("selin456", "123123123")
+			  ]
+
+	for i in veriler:
+		im.execute("""INSERT INTO users VALUES (?, ?)""", i)
+
+	db.commit()
+
+	
+def test(username,password):	
+	db = sqlite3.connect("mydb.db")
+
+	im = db.cursor()
+
+	im.execute("""SELECT * FROM users WHERE
+	username = ? AND password = ?""", (username, password))
+
+	data = im.fetchone()
+
+	if data:
+		print (u"Welcome %s!" % data[0])
+
+	else:
+		print (u"Wrong username or password!")
+		
+if __name__ == "__main__":
+ # if you call this script from the command line (the shell) it will
+ # run the 'main' function
+	create()
+	test()
