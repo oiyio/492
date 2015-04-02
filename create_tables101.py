64cafc779f9953811cb36a2d4244d801ab4dbd24
oiyio <omeriyioz@gmail.com>
2015-04-02 19:30:59 +0300

    users table, creation, insertion, testing authentication and printAll. Fields of primary key can be inserted without any problem.

--- create_tables101.py
+++ create_tables101.py
@@ -1,51 +1,82 @@
 # -*- coding: utf-8 -*-
-# TESTING
-#  import create_tables100 as cr
-#  cr.create()
-#  cr.test("ahmet123","12345678")
+######   testing in shell ######
+#####
+# import create_tables101 as cr
+# cr.create()
+# cr.insert()
+# cr.test("ali","123")
+# cr.printAll()
 
 import sqlite3
 
 def create():
-	db = sqlite3.connect("mydb.db")
+	db = sqlite3.connect("mydb2.db")
 
 	im = db.cursor()
 
-	im.execute("""CREATE TABLE users3(
-					username VARCHAR(20) NOT NULL, 
+	im.execute("""CREATE TABLE users8(
+					user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+					username VARCHAR(20) NOT NULL,
 					password VARCHAR(20) NOT NULL,
-					password2 VARCHAR(20) NOT NULL,
-					password3 VARCHAR(20) NOT NULL)""")
+					fullname VARCHAR(30) NOT NULL,
+					email VARCHAR(30) NOT NULL
+					)""")
+					
+def insert():
 
+	db = sqlite3.connect("mydb2.db")
+	im = db.cursor()
+	
 	veriler = [
-				("ahmet123", "12345678","12345678","12345678"),
-				("mehmet321", "87654321","12345678","12345678"),
-				("selin456", "123123123","12345678","12345678")
+				("ali","123","sanane","some@some"),
+				("omer","123","sanane","some@some"),
+				("ahmet","123","sanane","some@some"),
+				("huseyin","123","sanane","some@some"),
+				("kemal","123","sanane","some@some"),
+				("cagdas","123","sanane","some@some")
 			  ]
-
+	#skip user_id, bacause of autoincrement, thus NULL in mysql command.
 	for i in veriler:
-		im.execute("""INSERT INTO users3 VALUES (?, ?, ?, ?)""", i)
+		im.execute("""INSERT INTO users8 
+		VALUES (NULL, ?, ?, ?, ?)""", i)
 
 	db.commit()
 
+	
 def test(username,password):	
-	db = sqlite3.connect("mydb.db")
+	db = sqlite3.connect("mydb2.db")
 
 	im = db.cursor()
 
-	im.execute("""SELECT * FROM users3 WHERE
+	im.execute("""SELECT * FROM users8 WHERE
 	username = ? AND password = ?""", (username, password))
 
 	data = im.fetchone()
 
 	if data:
-		print (u"Welcome %s!" % data[0])
+		print (u"Welcome %s !" % data[1])
 
 	else:
 		print (u"Wrong username or password!")
-		
+	
+# -*- coding: utf-8 -*-
+
+def printAll():
+	vt = sqlite3.connect("mydb2.db")
+
+	im = vt.cursor()
+
+	im.execute("""SELECT * FROM users8""")
+
+	veriler = im.fetchall()
+
+	print (veriler)
+
+	
 if __name__ == "__main__":
  # if you call this script from the command line (the shell) it will
  # run the 'main' function
 	create()
 	test()
+	insert()
+	printAll()
